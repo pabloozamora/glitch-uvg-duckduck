8747da2ce99e281376963cdb05787f5160047c99
pabloozamora <pabloozamora02@gmail.com>
2023-03-09 20:05:50 -0600

    Componentes Square Message agregados a App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import laptop from './assets/laptop.svg'
+import search from './assets/search.svg'
+import mobile from './assets/mobile.svg'
 import './App.css'
 import { MessageSquare } from './components/MessageSquare/MessageSquare'
 
@@ -8,10 +11,15 @@ function App() {
   const description1 = "Navega como siempre y nosotros nos encargamos del resto. Te ofrecemos buscador, bloqueador de rastreadores y mejora de encriptación todo en una descarga, para los"
   const description2 = "Busca de forma privada con nuestra app o extensión, añade búsqueda web privada a tu navegador preferido o busca en"
   const title2 = "Motor de Búsqueda Privada"
+  const title3 = "Navegador Privado"
+  const description3 = "Nuestro navegador privado para móviles viene equipado con nuestro motor de búsqueda, bloqueador de rastreadores, mejora de encriptación y más. Disponible para"
   return (
     <div className="App">
-      <MessageSquare title = {title1} description={description1} link="https://duckduckgo.com/app" linkText="principales navegadores."/>
-      <MessageSquare title = {title2} description={description2} link="https://duckduckgo.com/" linkText="duckduckgo.com."/>
+      <div className='SquareMessageWrapper'>
+        <MessageSquare icon = {laptop} title = {title1} description={description1} link="https://duckduckgo.com/app" linkText="principales navegadores."/>
+        <MessageSquare icon = {search} title = {title2} description={description2} link="https://duckduckgo.com/" linkText="duckduckgo.com."/>
+        <MessageSquare icon = {mobile} title = {title3} description={description3} link="https://duckduckgo.com/app" linkText="iOS y Android."/>
+      </div>
     </div>
   )
 }
--- src/components/MessageSquare/MessageSquare.jsx
+++ src/components/MessageSquare/MessageSquare.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './MessageSquare.css'
 
 const MessageSquare = ({ icon, title, description, link, linkText }) => (
-    <div>
+    <div className = "square">
+        <img id = "icon" src={icon}></img>
         <h1>{title}</h1>
-        <p>{description}</p>
+        <p>{`${description} `}
         <a href={link}>{linkText}</a>
+        </p>
     </div>
   )
 
